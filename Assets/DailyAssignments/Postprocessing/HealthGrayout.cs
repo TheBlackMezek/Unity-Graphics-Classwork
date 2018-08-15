4fa49acd5e3499a60d6bec73a14439c0956f6f09
Alexander Thomas <6ftDwarf@gmail.com>
2018-08-14 17:07:37 -0700

    Add day's work

---
+++ Assets/DailyAssignments/Postprocessing/HealthGrayout.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class HealthGrayout : MonoBehaviour {
+
+    public float health;
+    private int healthDir = 1;
+
+    private PostProcessVolume volume;
+    private ColorGrading cg;
+
+    private void Start()
+    {
+        cg = ScriptableObject.CreateInstance<ColorGrading>();
+        cg.enabled.Override(true);
+        //cg.saturation.Override(-100f);
+
+        volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, cg);
+    }
+
+    private void Update()
+    {
+        if(health <= 0)
+        {
+            healthDir = 1;
+        }
+        else if(health >= 100)
+        {
+            healthDir = -1;
+        }
+
+        health += Time.deltaTime * healthDir * 100.0f;
+
+        cg.saturation.Override((100f - health) * -1);
+    }
+
+    private void OnDestroy()
+    {
+        RuntimeUtilities.DestroyVolume(volume, true, true);
+    }
+
+}
---
+++ Assets/DailyAssignments/Postprocessing/PPFadeToBlack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PPFadeToBlack : MonoBehaviour {
+
+    public float duration;
+    public bool fadeOnStart;
+
+    private PostProcessVolume volume;
+    private ColorGrading cg;
+    private bool fading = false;
+
+    private void Start()
+    {
+        cg = ScriptableObject.CreateInstance<ColorGrading>();
+        cg.enabled.Override(true);
+        
+        if(fadeOnStart)
+        {
+            fading = true;
+        }
+        
+        volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, cg);
+    }
+
+    private void Update()
+    {
+        if(fading)
+        {
+            cg.mixerBlueOutBlueIn.Override(cg.mixerBlueOutBlueIn.value - Time.deltaTime * (100f / duration));
+            cg.mixerRedOutRedIn.Override(cg.mixerRedOutRedIn.value - Time.deltaTime * (100f / duration));
+            cg.mixerGreenOutGreenIn.Override(cg.mixerGreenOutGreenIn.value - Time.deltaTime * (100f / duration));
+            if(cg.mixerBlueOutBlueIn <= 0)
+            {
+                fading = false;
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        RuntimeUtilities.DestroyVolume(volume, true, true);
+    }
+
+}
